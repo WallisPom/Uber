1f296edbb84cac27a7b48bfa9aa05e633010fe81
Pom <15wp@caslab.queensu.ca>
2018-12-23 22:07:34 -0500

    commit

---
+++ src/D1.java
@@ -0,0 +1,8 @@
+// Driver 1 class
+public class D1 {
+	public int Loc;
+	
+	public int Loc1() {
+		return this.Loc;
+	}
+}
---
+++ src/D2.java
@@ -0,0 +1,8 @@
+// Driver 2 class
+public class D2 {
+	public int Loc;
+	
+	public int Loc2() {
+		return this.Loc;
+	}
+}
